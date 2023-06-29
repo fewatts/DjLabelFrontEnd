a87995e9a942b0a1e8ddbff9fe1a6a3575b58204
Fewatts <feralveswatts@gmail.com>
2023-06-29 19:05:28 -0300

    Task 1
    Base do projeto + 3 components. Confirmar paleta e fontes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import gitlogo from './../../assets/github.svg'
 export function Footer() {
     return (
         <footer>
-            <a href="https://github.com/fewatts/tcc-cloud-school" target='blank'><abbr title="repositório no github"><img src={gitlogo} alt="Repositório" className='git' /></abbr></a>
+            <a href="https://github.com/fewatts/tetaseshinfos" target='blank'><abbr title="repositório no github"><img src={gitlogo} alt="Repositório" className='git' /></abbr></a>
             <p>© Copyright, F</p>
         </footer>
     );
