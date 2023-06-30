66a9e47bf433d11e4478362890c6dede931b3171
Fewatts <feralveswatts@gmail.com>
2023-06-30 10:22:04 -0300

    Task 2
    Info OK

--- src/pages/info/Info.tsx
+++ src/pages/info/Info.tsx
@@ -4,11 +4,13 @@ export function Info() {
     return (
         <>
             <h1 className='h1info'>Dj's TetaSesh:</h1>
+            <h3 className='h3info'>A - Z</h3>
             <main className='info'>
                 <div className='infobox'>
                     <div className='card'>
                         <img src='https://i1.sndcdn.com/avatars-XNvFE6BT4CmhlhVY-6qpkHQ-t500x500.jpg' alt="CostaI" className='profilepic' />
-                        <h1 className='h1but'>CostaI | House</h1>
+                        <h1 className='h1but'>CostaI</h1>
+                        <h3><strong>DeepTech</strong></h3>
                         <div className='divBut'>
 
                             <button className='butGetCv'><a href='https://soundcloud.com/costaimusic' target='blank'>SoundCloud</a></button>
@@ -20,7 +22,8 @@ export function Info() {
                 <div className='infobox'>
                     <div className='card'>
                         <img src='https://i1.sndcdn.com/avatars-xkz1oQLLXfpKeEZf-LTaThQ-t500x500.jpg' alt="Dabague" className='profilepic' />
-                        <h1 className='h1but'>Dabague | PsyTrance</h1>
+                        <h1 className='h1but'>Dabague</h1>
+                        <h3><strong>PsyTrance</strong></h3>
                         <div className='divBut'>
 
                             <button className='butGetCv'><a href='https://soundcloud.com/user-654415983' target='blank'>SoundCloud</a></button>
@@ -32,7 +35,8 @@ export function Info() {
                 <div className='infobox'>
                     <div className='card'>
                         <img src='https://i1.sndcdn.com/avatars-mNAxqOghXvLnDEiO-l73tUg-t500x500.jpg' alt="Laomer" className='profilepic' />
-                        <h1 className='h1but'>Laomer | House</h1>
+                        <h1 className='h1but'>Laomer</h1>
+                        <h3><strong>House</strong></h3>
                         <div className='divBut'>
 
                             <button className='butGetCv'><a href='https://soundcloud.com/laomer' target='blank'>SoundCloud</a></button>
@@ -44,7 +48,8 @@ export function Info() {
                 <div className='infobox'>
                     <div className='card'>
                         <img src='https://i1.sndcdn.com/avatars-PLUYBW7LFwGAn2vR-UOrM1A-t500x500.jpg' alt="Oghthraxx" className='profilepic' />
-                        <h1 className='h1but'>Oghthraxx | Techno</h1>
+                        <h1 className='h1but'>Oghthraxx</h1>
+                        <h3><strong>Techno & Phonk</strong></h3>
                         <div className='divBut'>
 
                             <button className='butGetCv'><a href='https://soundcloud.com/jovemthraxx' target='blank'>SoundCloud</a></button>
@@ -56,7 +61,8 @@ export function Info() {
                 <div className='infobox'>
                     <div className='card'>
                         <img src='https://i1.sndcdn.com/avatars-ptppwi5a88f6rK1y-bzyGcQ-t500x500.jpg' alt="SpinnenKopf" className='profilepic' />
-                        <h1 className='h1but'>SpinnenKopf | Techno</h1>
+                        <h1 className='h1but'>SpinnenKopf</h1>
+                        <h3><strong>Techno</strong></h3>
                         <div className='divBut'>
 
                             <button className='butGetCv'><a href='https://soundcloud.com/user-831177117' target='blank'>SoundCloud</a></button>
@@ -68,7 +74,8 @@ export function Info() {
                 <div className='infobox'>
                     <div className='card'>
                         <img src='https://i1.sndcdn.com/avatars-QVoUubV5OVX1nbiT-yyXy0A-t500x500.jpg' alt="Teta" className='profilepic' />
-                        <h1 className='h1but'>Teta | House</h1>
+                        <h1 className='h1but'>Bem4venturado</h1>
+                        <h3><strong>House</strong></h3>
                         <div className='divBut'>
 
                             <button className='butGetCv'><a href='https://soundcloud.com/enzo-beteto-5418300' target='blank'>SoundCloud</a></button>
@@ -80,7 +87,8 @@ export function Info() {
                 <div className='infobox'>
                     <div className='card'>
                         <img src='https://i1.sndcdn.com/avatars-L4EPEPnRPkxui7lS-5vUGKQ-t500x500.jpg' alt="Zanqueta" className='profilepic' />
-                        <h1 className='h1but'>Zanqueta | PsyTrance</h1>
+                        <h1 className='h1but'>Zanqueta</h1>
+                        <h3><strong>PsyTechno</strong></h3>
                         <div className='divBut'>
 
                             <button className='butGetCv'><a href='https://soundcloud.com/gustavo-moreira-933671017' target='blank'>SoundCloud</a></button>
