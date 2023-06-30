93e72b6a3287bd166d08af5ca0cc205f0c4ba073
Fewatts <feralveswatts@gmail.com>
2023-06-30 12:03:30 -0300

    Task 3
    Branch blue ok

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
 import './Home.css';
 import home from './../../../src/assets/home.svg';
+import soundcloud from './../../assets/soundcloud.svg';
+import youtube from './../../assets/youtube.svg';
 
 export function Home() {
     return (
@@ -8,6 +10,12 @@ export function Home() {
                 <h1 className='h1home'>Bem-vindo ao <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong>!</h1>
                 <h2>Este é o nosso site de divulgação de Conteúdo dos artistas do <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong>.</h2>
                 <p>Use a barra de navegação para acessar as informações c:</p>
+                <div className='out'>
+                    {/* <a href="https://soundcloud.com/tetasesh" target='blank' ></a> */}
+                    <img src={soundcloud} alt="Soundcloud" className='soundcloud'/>
+                    <img src={youtube} alt="youtube" className='youtube'/>
+                    {/* <a href="https://www.youtube.com/@TetaSesh" target='blank' ></a> */}
+                </div>
             </div>
             <div className='homepic'><img src={home} alt="pic home" className='Homepic' /></div>
         </main>
