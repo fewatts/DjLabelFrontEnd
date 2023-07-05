c199bfbdf455947a7a8a2c4b46bbf8537c761b49
Fewatts <feralveswatts@gmail.com>
2023-07-05 10:38:13 -0300

    Refactor: Info and register

--- src/pages/info/Info.tsx
+++ src/pages/info/Info.tsx
@@ -1,5 +1,57 @@
 import './Info.css';
 
+const artists = [
+    {
+        name: 'Bem4venturado',
+        genre: 'House',
+        profilePic: 'https://i1.sndcdn.com/avatars-QVoUubV5OVX1nbiT-yyXy0A-t500x500.jpg',
+        soundcloudLink: 'https://soundcloud.com/enzo-beteto-5418300',
+        instagramLink: 'https://www.instagram.com/bem4venturado/'
+    },
+    {
+        name: 'CostaI',
+        genre: 'DeepTech',
+        profilePic: 'https://i1.sndcdn.com/avatars-XNvFE6BT4CmhlhVY-6qpkHQ-t500x500.jpg',
+        soundcloudLink: 'https://soundcloud.com/costaimusic',
+        instagramLink: 'https://www.instagram.com/costai.wav/'
+    },
+    {
+        name: 'Dabague',
+        genre: 'PsyTrance',
+        profilePic: 'https://i1.sndcdn.com/avatars-xkz1oQLLXfpKeEZf-LTaThQ-t500x500.jpg',
+        soundcloudLink: 'https://soundcloud.com/user-654415983',
+        instagramLink: 'https://www.instagram.com/dabague_/'
+    },
+    {
+        name: 'Laomer',
+        genre: 'House',
+        profilePic: 'https://i1.sndcdn.com/avatars-mNAxqOghXvLnDEiO-l73tUg-t500x500.jpg',
+        soundcloudLink: 'https://soundcloud.com/laomer',
+        instagramLink: 'https://www.instagram.com/quedorlaomer/'
+    },
+    {
+        name: 'Oghthraxx',
+        genre: 'Techno & Phonk',
+        profilePic: 'https://i1.sndcdn.com/avatars-PLUYBW7LFwGAn2vR-UOrM1A-t500x500.jpg',
+        soundcloudLink: 'https://soundcloud.com/jovemthraxx',
+        instagramLink: 'https://www.instagram.com/ogthraxx1/'
+    },
+    {
+        name: 'SpinnenKopf',
+        genre: 'Techno',
+        profilePic: 'https://i1.sndcdn.com/avatars-ptppwi5a88f6rK1y-bzyGcQ-t500x500.jpg',
+        soundcloudLink: 'https://soundcloud.com/user-831177117',
+        instagramLink: 'https://www.instagram.com/spinnenkopf_/'
+    },
+    {
+        name: 'Zanqueta',
+        genre: 'PsyTechno',
+        profilePic: 'https://i1.sndcdn.com/avatars-L4EPEPnRPkxui7lS-5vUGKQ-t500x500.jpg',
+        soundcloudLink: 'https://soundcloud.com/gustavo-moreira-933671017',
+        instagramLink: 'https://www.instagram.com/guszanqueta/'
+    }
+];
+
 export function Info() {
     return (
         <>
@@ -9,98 +61,20 @@ export function Info() {
             </div>
             <main className='info'>
 
-            <div className='infobox'>
-                    <div className='card'>
-                        <img src='https://i1.sndcdn.com/avatars-QVoUubV5OVX1nbiT-yyXy0A-t500x500.jpg' alt="Teta" className='profilepic' />
-                        <h2 className='h1but'>Bem4venturado</h2>
-                        <h3><strong>House</strong></h3>
-                        <div className='divBut'>
-
-                            <button className='butGetCv'><a href='https://soundcloud.com/enzo-beteto-5418300' target='blank'>SoundCloud</a></button>
-                            <button className='butGetCv'><a href='https://www.instagram.com/bem4venturado/' target='blank'>Instagram</a></button>
+                {artists.map(artist => (
+                    <div className='infobox'>
+                        <div className='card'>
+                            <img src={artist.profilePic} alt={artist.name} className='profilepic' />
+                            <h2 className='h1but'>{artist.name}</h2>
+                            <h3><strong>{artist.genre}</strong></h3>
+                            <div className='divBut'>
+                                <button className='butGetCv'><a href={artist.soundcloudLink} target='blank'>SoundCloud</a></button>
+                                <button className='butGetCv'><a href={artist.instagramLink} target='blank'>Instagram</a></button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
 
-                
-                <div className='infobox'>
-                    <div className='card'>
-                        <img src='https://i1.sndcdn.com/avatars-XNvFE6BT4CmhlhVY-6qpkHQ-t500x500.jpg' alt="CostaI" className='profilepic' />
-                        <h2 className='h1but'>CostaI</h2>
-                        <h3><strong>DeepTech</strong></h3>
-                        <div className='divBut'>
-
-                            <button className='butGetCv'><a href='https://soundcloud.com/costaimusic' target='blank'>SoundCloud</a></button>
-                            <button className='butGetCv'><a href='https://www.instagram.com/costai.wav/' target='blank'>Instagram</a></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='infobox'>
-                    <div className='card'>
-                        <img src='https://i1.sndcdn.com/avatars-xkz1oQLLXfpKeEZf-LTaThQ-t500x500.jpg' alt="Dabague" className='profilepic' />
-                        <h2 className='h1but'>Dabague</h2>
-                        <h3><strong>PsyTrance</strong></h3>
-                        <div className='divBut'>
-
-                            <button className='butGetCv'><a href='https://soundcloud.com/user-654415983' target='blank'>SoundCloud</a></button>
-                            <button className='butGetCv'><a href='https://www.instagram.com/dabague_/' target='blank'>Instagram</a></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='infobox'>
-                    <div className='card'>
-                        <img src='https://i1.sndcdn.com/avatars-mNAxqOghXvLnDEiO-l73tUg-t500x500.jpg' alt="Laomer" className='profilepic' />
-                        <h2 className='h1but'>Laomer</h2>
-                        <h3><strong>House</strong></h3>
-                        <div className='divBut'>
-
-                            <button className='butGetCv'><a href='https://soundcloud.com/laomer' target='blank'>SoundCloud</a></button>
-                            <button className='butGetCv'><a href='https://www.instagram.com/quedorlaomer/' target='blank'>Instagram</a></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='infobox'>
-                    <div className='card'>
-                        <img src='https://i1.sndcdn.com/avatars-PLUYBW7LFwGAn2vR-UOrM1A-t500x500.jpg' alt="Oghthraxx" className='profilepic' />
-                        <h2 className='h1but'>Oghthraxx</h2>
-                        <h3><strong>Techno & Phonk</strong></h3>
-                        <div className='divBut'>
-
-                            <button className='butGetCv'><a href='https://soundcloud.com/jovemthraxx' target='blank'>SoundCloud</a></button>
-                            <button className='butGetCv'><a href='https://www.instagram.com/ogthraxx1/' target='blank'>Instagram</a></button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='infobox'>
-                    <div className='card'>
-                        <img src='https://i1.sndcdn.com/avatars-ptppwi5a88f6rK1y-bzyGcQ-t500x500.jpg' alt="SpinnenKopf" className='profilepic' />
-                        <h2 className='h1but'>SpinnenKopf</h2>
-                        <h3><strong>Techno</strong></h3>
-                        <div className='divBut'>
-
-                            <button className='butGetCv'><a href='https://soundcloud.com/user-831177117' target='blank'>SoundCloud</a></button>
-                            <button className='butGetCv'><a href='https://www.instagram.com/spinnenkopf_/' target='blank'>Instagram</a></button>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className='infobox'>
-                    <div className='card'>
-                        <img src='https://i1.sndcdn.com/avatars-L4EPEPnRPkxui7lS-5vUGKQ-t500x500.jpg' alt="Zanqueta" className='profilepic' />
-                        <h2 className='h1but'>Zanqueta</h2>
-                        <h3><strong>PsyTechno</strong></h3>
-                        <div className='divBut'>
-
-                            <button className='butGetCv'><a href='https://soundcloud.com/gustavo-moreira-933671017' target='blank'>SoundCloud</a></button>
-                        </div>
-                        <button className='butGetCv'><a href='https://www.instagram.com/guszanqueta/' target='blank'>Instagram</a></button>
-                    </div>
-                </div>
             </main>
         </>
     );
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,7 +1,7 @@
+import { ChangeEvent, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { cadastrarUsuario } from '../../service/Service';
 import './Register.css';
-import { ChangeEvent, useState } from 'react';
 import { Customer } from '../../models/Model';
 import { toast } from 'react-toastify';
 
@@ -12,15 +12,15 @@ export function Register() {
 
     const [usuario, setUsuario] = useState<Customer>({
         id: 0,
-        name: "",
-        email: "",
-        number: ""
+        name: '',
+        email: '',
+        number: ''
     });
 
     function updateModel(event: ChangeEvent<HTMLInputElement>) {
         setUsuario({
             ...usuario,
-            [event.target.name]: event.target.value,
+            [event.target.name]: event.target.value
         });
     }
 
@@ -33,82 +33,89 @@ export function Register() {
                 console.log(usuario);
                 await cadastrarUsuario('/customer/register', usuario, setUsuario);
                 toast.success('Usuário cadastrado!', {
-                    position: "top-center",
+                    position: 'top-center',
                     autoClose: 4000,
                     hideProgressBar: false,
                     closeOnClick: true,
                     pauseOnHover: true,
                     draggable: true,
                     progress: undefined,
-                    theme: "colored",
+                    theme: 'colored'
                 });
-                navigate("/");
+                navigate('/');
             } catch (error) {
                 setIsLoading(false);
-                toast.error('Usamos hospedagem de backend gratuito, as vezes o sistema hiberna\nGeralmente na segunda vez funciona, \ntenta de novo!', {
-                    position: "top-center",
+                toast.error(
+                    'Usamos hospedagem de backend gratuito, as vezes o sistema hiberna\nGeralmente na segunda vez funciona, \ntenta de novo!',
+                    {
+                        position: 'top-center',
+                        autoClose: 4000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: 'colored'
+                    }
+                );
+            }
+        } else {
+            toast.warning(
+                'Preencheu tudo certo?\nColoque seu nome, um email válido\nE um número válido...',
+                {
+                    position: 'top-center',
                     autoClose: 4000,
                     hideProgressBar: false,
                     closeOnClick: true,
                     pauseOnHover: true,
                     draggable: true,
                     progress: undefined,
-                    theme: "colored",
-                });
-            }
-        } else {
-            toast.warning('Preencheu tudo certo?\nColoque seu nome, um email válido\nE um número válido...', {
-                position: "top-center",
-                autoClose: 4000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "colored",
-            });
+                    theme: 'colored'
+                }
+            );
         }
     }
 
     return (
         <>
-            <h2 className='h3register'>Cadastrar</h2>
+            <h2 className="h3register">Cadastrar</h2>
             <div className="form-container">
-                <form onSubmit={onSubmit} className='form'>
+                <form onSubmit={onSubmit} className="form">
                     <h3>Nome:</h3>
                     <input
-                        placeholder='Nome'
-                        className='input'
+                        placeholder="Nome"
+                        className="input"
                         type="text"
                         name="name"
-                        onChange={(event: ChangeEvent<HTMLInputElement>) => updateModel(event)}
+                        value={usuario.name}
+                        onChange={updateModel}
                     />
 
                     <h3>E-mail:</h3>
                     <input
-                        placeholder='seuemail@example.com'
-                        className='input'
+                        placeholder="seuemail@example.com"
+                        className="input"
                         type="email"
                         name="email"
-                        onChange={(event: ChangeEvent<HTMLInputElement>) => updateModel(event)}
+                        value={usuario.email}
+                        onChange={updateModel}
                     />
 
                     <h3>Número:</h3>
                     <input
-                        placeholder='(xx) xxxxxxxxx'
-                        className='input'
+                        placeholder="(xx) xxxxxxxxx"
+                        className="input"
                         type="text"
                         name="number"
-                        onChange={(event: ChangeEvent<HTMLInputElement>) => updateModel(event)}
+                        value={usuario.number}
+                        onChange={updateModel}
                     />
-                    <div className='butscad'>
-                        <button disabled={isLoading} className='butsubmit'>
-                            {isLoading ? (<span >Loading..</span>) : ('Cadastrar')}
+                    <div className="butscad">
+                        <button disabled={isLoading} className="butsubmit">
+                            {isLoading ? <span>Loading..</span> : 'Cadastrar'}
                         </button>
-                        <Link to='/'>
-                            <button className='butcan'>
-                                Cancelar
-                            </button>
+                        <Link to="/">
+                            <button className="butcan">Cancelar</button>
                         </Link>
                     </div>
                 </form>
@@ -116,3 +123,4 @@ export function Register() {
         </>
     );
 }
+``
