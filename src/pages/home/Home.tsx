8db0ac6ffb6373b72bb34a38858a9e34e69e878f
Fewatts <feralveswatts@gmail.com>
2023-07-10 11:59:58 -0300

    ajustes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,31 +2,32 @@ import './Home.css';
 import home from './../../../src/assets/home.svg';
 import soundcloud from './../../assets/soundcloud.svg';
 import youtube from './../../assets/youtube.svg';
-import { useNavigate } from 'react-router-dom';
+import { Sets } from '../sets/Sets';
 
 export function Home() {
 
-    const navigate = useNavigate();
-
-    function register(){
-        navigate("/register")
-    }
-
     return (
-        <main>
-            <div className='container'>
-                <h1 className='h1home'>Bem-vindo!</h1>
-                <h4 className='texthome'>Este é o nosso site de divulgação de Conteúdo dos artistas do <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong>. C:</h4>
-                
-                <div className='out'>
-                    <button className='butcad' onClick={register}>Cadastrar</button>
+        <>
+            <main>
+                <div className='container'>
+                    <h1 className='h1home'>Bem-vindo!</h1>
+                    <h4 className='texthome'>Este é o nosso site de divulgação de Conteúdo dos artistas do <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong>. C:</h4>
 
-                    <a href="https://soundcloud.com/tetasesh" target='blank' ><img src={soundcloud} alt="Soundcloud" className='soundcloud' /></a>
+                    <div className='out'>
 
-                    <a href="https://www.youtube.com/@TetaSesh" target='blank' ><img src={youtube} alt="youtube" className='youtube' /></a>
+                        <a href="https://soundcloud.com/tetasesh" target='blank' ><img src={soundcloud} alt="Soundcloud" className='soundcloud' /></a>
+
+                        <a href="https://www.youtube.com/@TetaSesh" target='blank' ><img src={youtube} alt="youtube" className='youtube' /></a>
+                    </div>
                 </div>
+                <div className='homepic'><img src={home} alt="pic home" className='Homepic' /></div>
+            </main>
+            <div className='splithome'>
+                <h1 className='h1ouca'>Ouça:</h1>
             </div>
-            <div className='homepic'><img src={home} alt="pic home" className='Homepic' /></div>
-        </main>
+            <section>
+                <Sets/>
+            </section>
+        </>
     );
 }
