93ec38b7517a5f29194c207205f88a319c66f245
Fewatts <feralveswatts@gmail.com>
2023-07-11 16:12:10 -0300

    Ajustes: Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import './Home.css';
-import home from './../../../src/assets/home.svg';
+import home from './../../../src/assets/home.jpg';
 import soundcloud from './../../assets/soundcloud.svg';
 import youtube from './../../assets/youtube.svg';
 import { Sets } from '../sets/Sets';
@@ -10,23 +10,23 @@ export function Home() {
         <>
             <main>
                 <div className='container'>
-                    <h1 className='h1home'>Bem-vindo!</h1>
-                    <h4 className='texthome'>Este é o nosso site de divulgação de Conteúdo dos artistas do <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong>. C:</h4>
-
-                    <div className='out'>
-
-                        <a href="https://soundcloud.com/tetasesh" target='blank' ><img src={soundcloud} alt="Soundcloud" className='soundcloud' /></a>
-
-                        <a href="https://www.youtube.com/@TetaSesh" target='blank' ><img src={youtube} alt="youtube" className='youtube' /></a>
+                    <div className='textos'>
+                        <h1 className='h1home'>Bem-vindo!</h1>
+                        <h4 className='texthome'>Este é o nosso site de divulgação de Conteúdo dos artistas do <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong>. C:</h4>
+                        <div className='out'>
+                            <a href="https://soundcloud.com/tetasesh" target='blank' ><img src={soundcloud} alt="Soundcloud" className='soundcloud' /></a>
+                            <a href="https://www.youtube.com/@TetaSesh" target='blank' ><img src={youtube} alt="youtube" className='youtube' /></a>
+                        </div>
                     </div>
                 </div>
-                <div className='homepic'><img src={home} alt="pic home" className='Homepic' /></div>
+                <div className='homepic'><a href="https://www.freepik.com/free-vector/dj-tools-equipment-isometric-set-with-audio-mixer-headphone-vinyl-record-player-speakers-instruments-isolated-vector-illustration_26765259.htm#page=2&query=dj&position=12&from_view=search&track=sph"><abbr title="Image by macrovector"><img src={home} alt="pic home" className='Homepic' /></abbr></a></div>
             </main>
+            <hr className='hrhome' />
             <div className='splithome'>
                 <h1 className='h1ouca'>Ouça:</h1>
             </div>
             <section>
-                <Sets/>
+                <Sets />
             </section>
         </>
     );
