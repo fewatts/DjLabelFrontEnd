0ed896b2db823a3dd71192203eef83176713f042
Fewatts <feralveswatts@gmail.com>
2023-08-01 09:35:39 -0300

    part 2: Navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -6,10 +6,10 @@ export function NavBar() {
     return (
         <header>
             <nav className='navbar'>
-                <picture className='logo-container'>
+                <picture className='logo-navbar'>
                     <Link to='/'><img src={logo} alt="logo" className='logo' /></Link>
                 </picture>
-                <ul id='itens2'>
+                <ul id='nav-links'>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/info'>Sociais</Link></li>
                     <li><Link to='/register'>Cadastro</Link></li>
