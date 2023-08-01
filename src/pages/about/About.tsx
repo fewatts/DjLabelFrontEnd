e5748aab91e4a14cff22403ba22bb173bf4526a6
Fewatts <feralveswatts@gmail.com>
2023-08-01 13:24:12 -0300

    part 6: Home, error, about OK

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,15 +2,15 @@ import './About.css';
 
 export function About() {
     return (
-        <main className="about">
-            <section id='sh'>
-                <h1 id='h1about'>Sobre nós</h1>
-                <p className='para'>
+        <main className="main-about">
+            <article id='article-about'>
+                <h1 id='tittle-about'>Sobre nós</h1>
+                <p className='paragrafh-about'>
                     Bem-vindo ao nosso site, onde você encontrará informações sobre o <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong>, um grupo de DJs apaixonados pela música eletrônica. Somos um coletivo formado por talentosos artistas que buscam proporcionar experiências sonoras únicas e envolventes para o público.
                     Nossos DJs têm estilos distintos que abrangem diversas vertentes da música eletrônica. Conheça nossos artistas:
                 </p>
                 <br />
-                <h3 id='h1about'>A - Z</h3>
+                <h3 id='tittle-about'>A - Z</h3>
                 <ul>
                     <li>
                         <strong>Costai</strong>: Toca <strong>deephouse</strong>.
@@ -29,16 +29,16 @@ export function About() {
                     </li>
                 </ul>
                 <br />
-                <p className='para'>
+                <p className='paragrafh-about'>
                     Nosso formato de eventos e resenhas é projetado para oferecer ao público uma diversidade de estilos musicais. Cada um de nossos DJs tem a oportunidade de mostrar sua arte durante uma hora de set, proporcionando uma experiência única e dinâmica. Nosso objetivo é levar você a uma jornada sonora memorável, cheia de ritmo e energia.
                     Embora sejamos um coletivo de DJs iniciantes, estamos comprometidos em aprimorar constantemente nossas habilidades e levar nossa música a um público cada vez maior. Estamos abertos a novas oportunidades e colaborações, sempre buscando expandir nossos horizontes musicais.
                 </p>
                 <br />
-                <p className='para'>
+                <p className='paragrafh-about'>
                     Acompanhe nosso site para ficar atualizado sobre nossa agenda de eventos, resenhas e novidades. Estamos entusiasmados em compartilhar nossa paixão pela música eletrônica com você e estamos ansiosos para levar a energia do <strong><abbr title="Label de Artistas Dj's" className='abbrt'>Teta Sesh</abbr></strong> aos seus ouvidos.
                     Junte-se a nós nessa jornada musical e descubra o poder da música eletrônica em todas as suas formas!
                 </p>
-            </section>
+            </article>
         </main>
     );
 }
