d1ed6fc43f6bf9a5001f09b222a12f375f900133
Fewatts <feralveswatts@gmail.com>
2023-08-01 09:30:15 -0300

    part 1: Navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,25 +4,23 @@ import './NavBar.css';
 
 export function NavBar() {
     return (
-        <nav className='navbar'>
-            <div className='logo-container'>
-                <Link to='/'><img src={logo} alt="logo" className='logo' /></Link>
-            </div>
-
-            <div id='itens'>
+        <header>
+            <nav className='navbar'>
+                <picture className='logo-container'>
+                    <Link to='/'><img src={logo} alt="logo" className='logo' /></Link>
+                </picture>
                 <ul id='itens2'>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/info'>Sociais</Link></li>
                     <li><Link to='/register'>Cadastro</Link></li>
                     <li><Link to='/about'>Sobre</Link></li>
                 </ul>
-            </div>
-
-            <Link to='/menu'>
-                <span id='burguer' className="material-symbols-outlined">
-                    menu
-                </span>
-            </Link>
-        </nav>
+                <Link to='/menu'>
+                    <span id='burguer' className="material-symbols-outlined">
+                        menu
+                    </span>
+                </Link>
+            </nav>
+        </header>
     );
 }
