4b19510a18f571d4284bcfd3ae496f330087d88d
Fernando Alves de Paula <110425294+fewatts@users.noreply.github.com>
2023-10-30 15:44:37 -0300

    Update Sets.tsx

--- src/components/sets/Sets.tsx
+++ src/components/sets/Sets.tsx
@@ -10,7 +10,7 @@ const tracks = [
         genre: 'Techno'
     },
     {
-        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1652654688&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1652781372&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
         genre: 'Techno'
     },
     {
