0eb94f2ba25b7851b2e7bfdedd0d7f631d9950b7
Fewatts <feralveswatts@gmail.com>
2023-10-31 18:00:14 -0300

    update tracks

--- src/components/sets/Sets.tsx
+++ src/components/sets/Sets.tsx
@@ -6,15 +6,7 @@ const tracks = [
         genre: 'DeepTech'
     },
     {
-        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1682580825&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
-        genre: 'Techno'
-    },
-    {
-        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1652781372&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
-        genre: 'Techno'
-    },
-    {
-        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1652687598&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1714480659&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
         genre: 'Techno'
     },
     {
