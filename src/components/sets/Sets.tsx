edb8b9e67ff0e8e2f9f51e2d5411af9dda0ffa6b
Fewatts <feralveswatts@gmail.com>
2023-12-03 18:21:57 -0300

    update tracks

--- src/components/sets/Sets.tsx
+++ src/components/sets/Sets.tsx
@@ -6,7 +6,7 @@ const tracks = [
         genre: 'DeepTech'
     },
     {
-        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1732171482&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
+        link: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1732834716&color=%231f5673&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
         genre: 'Techno'
     },
     {
@@ -53,3 +53,4 @@ export function Sets() {
         </section>
     );
 }
+<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src=""></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/spinner-kopf" title="𝕾𝖕𝖎𝖓𝖓𝖊𝖓𝕶𝖔𝖕𝖋" target="_blank" style="color: #cccccc; text-decoration: none;">𝕾𝖕𝖎𝖓𝖓𝖊𝖓𝕶𝖔𝖕𝖋</a> · <a href="https://soundcloud.com/spinner-kopf/sets/ep-keima" title="EP: Keima" target="_blank" style="color: #cccccc; text-decoration: none;">EP: Keima</a></div>
